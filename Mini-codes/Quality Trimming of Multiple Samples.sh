bed0dd8100db26a457a8547c77338f20b22daad3
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-09-10 14:00:24 +0100

    Create Quality Trimming of Multiple Samples.sh

---
+++ Mini-codes/Quality Trimming of Multiple Samples.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create output directory
+mkdir -p qc_reads
+
+# Define array of sample names
+samples=(ACBarrie Alsen Baxter Chara Drysdale)
+
+# Loop through each sample and run fastp
+for SAMPLE in "${samples[@]}"; do
+    echo "Processing $SAMPLE..."
+
+    fastp \
+        -i ${SAMPLE}_R1.fastq.gz \
+        -I ${SAMPLE}_R2.fastq.gz \
+        -o qc_reads/${SAMPLE}_R1.fastq.gz \
+        -O qc_reads/${SAMPLE}_R2.fastq.gz \
+        -h qc_reads/${SAMPLE}_fastp.html \
+        -j qc_reads/${SAMPLE}_fastp.json \
+        --detect_adapter_for_pe \
+        --thread 4
+
+    echo "$SAMPLE done."
+done
+
+echo "All samples processed."
