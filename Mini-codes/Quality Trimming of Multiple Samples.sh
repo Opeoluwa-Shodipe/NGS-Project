7ea56ba7a69be63ded46bebb7a74cbd1d15e3b52
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-09-15 00:22:34 +0100

    Update Quality Trimming of Multiple Samples.sh

--- Mini-codes/Quality Trimming of Multiple Samples.sh
+++ Mini-codes/Quality Trimming of Multiple Samples.sh
@@ -23,4 +23,40 @@ for SAMPLE in "${samples[@]}"; do
     echo "$SAMPLE done."
 done
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+mkdir qc_reads
+SAMPLES=(
+  "ACBarrie"
+  "Alsen"
+  "Baxter"
+  "Chara"
+  "Drysdale"
+)
+
+for SAMPLE in "${SAMPLES[@]}"; do
+
+  fastp \
+    -i "$PWD/${SAMPLE}_R1.fastq.gz" \
+    -I "$PWD/${SAMPLE}_R2.fastq.gz" \
+    -o "qc_reads/${SAMPLE}_R1.fastq.gz" \
+    -O "qc_reads/${SAMPLE}_R2.fastq.gz" \
+    --html "qc_reads/${SAMPLE}_fastp.html" 
+done
+
 echo "All samples processed."
