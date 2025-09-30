377e4f3f75edf65a51b08a69d5c540162fc1c17b
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-09-30 23:42:58 +0100

    Update Raw Script.sh

--- Projects/STAGE 1/Raw Script.sh
+++ Projects/STAGE 1/Raw Script.sh
@@ -50,6 +50,10 @@ index_reference() {
   [ -f "${REF%.fa}.dict" ] || java -jar "$(which picard)" CreateSequenceDictionary R="$REF" O="${REF%.fa}.dict"
 }
 
+#1 To repair trimmed reads before aligning to reference genome
+repair.sh in1-trimmed_reads/child_1.fastq.gz in2-trimmed_reads/child_2.fastq.gz out1-child_1_rep.fastq.gz  out2-child_2_rep.fastq.gz outsingle-single.fq
+#2 To index
+bwa index reference/hg38/hg38.fasta
 
 # --- Alignment ---
 align_reads() {
@@ -61,9 +65,8 @@ align_reads() {
   bwa mem -t "$THREADS" "$REF" "$OUTDIR/father_1.trim.fastq.gz" "$OUTDIR/father_2.trim.fastq.gz" \
     | samtools view -b -@ "$THREADS" -o "$OUTDIR/father.unsorted.bam" -
 }
-#1 to repair trimmed reads before aligning to reference genome
-repair.sh in1-trimmed_reads/child_1.fastq.gz in2-trimmed_reads/child_2.fastq.gz out1-child_1_rep.fastq.gz  out2-child_2_rep.fastq.gz outsingle-single.fq
-#2 Alignment code
+
+# Alignment code
 bwa mem reference/hg38/hg38.fasta child_1_rep.fastq.gz child_2_rep.fastq.gz > alignment/child.sam
 
 # --- Sorting, Deduplication, Indexing ---
