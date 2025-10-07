8f0229a1565dd571bc077ee01bba96f1e3609392
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-10-07 19:39:21 +0100

    Delete Projects/STAGE 1/Bacteria.sh

--- Projects/STAGE 1/Bacteria.sh
+++
@@ -1,8 +0,0 @@
-Focused on [insert specific topics or sections if needed].
-
-Format each flashcard like: Q: [Question]
-A: [Answer]
-
-Github: https://github.com/Opeoluwa-Shodipe/NGS-Project-HACKBIO-/blob/main/Projects/STAGE 1/Bacteria.sh
-
-Linkedin: Video would be uploaded
