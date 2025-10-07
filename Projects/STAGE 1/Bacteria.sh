d39c2c8f7d211437473f4919024b06e81cde798c
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-10-07 19:39:03 +0100

    Update Bacteria.sh

--- Projects/STAGE 1/Bacteria.sh
+++ Projects/STAGE 1/Bacteria.sh
@@ -3,6 +3,6 @@ Focused on [insert specific topics or sections if needed].
 Format each flashcard like: Q: [Question]
 A: [Answer]
 
-Github: 
+Github: https://github.com/Opeoluwa-Shodipe/NGS-Project-HACKBIO-/blob/main/Projects/STAGE 1/Bacteria.sh
 
 Linkedin: Video would be uploaded
