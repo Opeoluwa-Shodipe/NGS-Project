a92ef72dc169f8b666a653fd87ea89817b34f6d7
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-10-13 23:48:36 +0100

    Create Mapping with STAR.sh

---
+++ Mini-codes/Mapping with STAR.sh
@@ -0,0 +1,25 @@
+#download genome
+wget http://ftp.ensemblgenomes.org/pub/metazoa/release-61/fasta/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz
+gunzip Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz 
+
+# rename the file to c_elegans.fa
+mv Caenorhabditis_elegans.WBcel235.dna.toplevel.fa c_elegans.fa
+# create genome index directory
+STAR --runMode genomeGenerate --genomeDir genomeIndex --genomeFastaFiles c_elegans.fa
+
+# create directories for output
+mkdir -p mapped
+#! /bin/bash
+for infile in trim/*.fq ; do
+	outfile=$(basename "$infile".fq)
+	STAR --genomeDir genome/genomeIndex --readFilesIn $infile --outFileNamePrefix mapped/$outfile --outSAMtype BAM SortedByCoordinate --outSAMattributes All
+done
+
+#play with the bam output
+samtools view mapped/fm_1.fq.fq_trim.fq.fqAligned.sortedByCoord.out.bam | head
+samtools view -c mapped/fm_1.fq.fq_trim.fq.fqAligned.sortedByCoord.out.bam | head
+samtools  flagstat mapped/fm_1.fq.fq_trim.fq.fqAligned.sortedByCoord.out.bam | head
+
+# create a directory for IGV viewing
+mkdir IGV
+cp mapped/*.bam IGV/
